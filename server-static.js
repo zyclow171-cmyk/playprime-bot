9c252e542dcc2dd0f52c92ea5cfb94d2677e8444
zyclow171-cmyk <zyclow171@gmail.com>
2026-03-11 11:28:04 -0300

    Update server-static.js

--- server-static.js
+++ server-static.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static(__dirname));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
+
+app.get('/health', (req, res) => res.send('OK'));
 
 app.listen(PORT, () => console.log(`Landing page rodando na porta ${PORT}`));
