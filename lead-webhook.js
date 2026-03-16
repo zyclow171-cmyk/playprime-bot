852a2b2cd3207113ae36310b31487ff007edac3b
zyclow171-cmyk <zyclow171@gmail.com>
2026-03-16 15:36:13 -0300

    rewrite complete

--- lead-webhook.js
+++ lead-webhook.js
@@ -1,14 +1,3 @@
-app.get('/webhook', (req, res) => {
-  const mode = req.query['hub.mode'];
-  const token = req.query['hub.verify_token'];
-  const challenge = req.query['hub.challenge'];
-  if (mode === 'subscribe' && token === 'playprime123') {
-    res.status(200).send(challenge);
-  } else {
-    res.sendStatus(403);
-  }
-});
-
 app.post('/webhook', (req, res) => {
   const body = req.body;
   if (body.object === 'whatsapp_business_account') {
