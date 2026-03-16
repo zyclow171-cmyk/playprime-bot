e13a89dc4d843f1a8a9ebad17390e78d0ec8cea1
zyclow171-cmyk <zyclow171@gmail.com>
2026-03-16 16:30:27 -0300

    improve bot responses

--- lead-webhook.js
+++ lead-webhook.js
@@ -18,26 +18,8 @@ app.get('/webhook', (req, res) => {
 });
 
 app.post('/webhook', async (req, res) => {
+  res.sendStatus(200);
   const body = req.body;
   if (body.object === 'whatsapp_business_account') {
     const message = body.entry?.[0]?.changes?.[0]?.value?.messages?.[0];
-    if (message) {
-      await sendMessage(message.from, 'Bem-vindo a PlayPrime IPTV! Digite 1 para ver nossos planos.');
-    }
-  }
-  res.sendStatus(200);
-});
-
-app.get('/', (req, res) => res.json({ status: 'online' }));
-
-async function sendMessage(to, text) {
-  const fetch = (...args) => import('node-fetch').then(({default: f}) => f(...args));
-  await fetch('https://graph.facebook.com/v18.0/' + NUMERO_ID + '/messages', {
-    method: 'POST',
-    headers: { 'Authorization': 'Bearer ' + WHATSAPP_TOKEN, 'Content-Type': 'application/json' },
-    body: JSON.stringify({ messaging_product: 'whatsapp', to, type: 'text', text: { body: text } })
-  });
-}
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log('Servidor na porta ' + PORT));
+    if
